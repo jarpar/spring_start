d68ff5064ecae8587fae5818082bb0d3a44b1195
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-12-06 12:42:06 +0100

    refactored

--- src/main/java/pl/sda/spring_start/controller/AdminController.java
+++ src/main/java/pl/sda/spring_start/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.spring_start.model.User;
 import pl.sda.spring_start.service.UserService;
@@ -22,5 +23,12 @@ public class AdminController {
         model.addAttribute("users", users);
         return "tables";
     }
-
+    @GetMapping("/users/addAdmin&{userId}")
+    public String addAdmin(@PathVariable("userId") int userId){
+        return null;
+    }
+    @GetMapping("/users/changeStatus&{userId}")
+    public String changeUserStatus(@PathVariable("userId") int userId){
+        return null;
+    }
 }
